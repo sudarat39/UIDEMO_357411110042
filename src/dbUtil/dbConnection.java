939a231a9fa8910a94369cc8c79fb12e4d39f2be
sudarat39 <beskunlasing@hotmail.com>
2018-02-20 12:23:42 +0700

    loginModel

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,15 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SOCONN ="jdbc:sqlite:school.sqlite";
-    private static Connection getConnection(){
+
+    private static final String SQCONN =
+            "jdbc:sqlite:school.sqlite";
+
+    public static Connection getConnection() throws SQLException {
         try {
-           Class.forName("org.sqlite.JDBC") ;
-           return DriverManager.getConnection(SOCONN);
-    } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
         return null;
-    }//class
+
+    }//getConnection
+
+
+
+}//class
